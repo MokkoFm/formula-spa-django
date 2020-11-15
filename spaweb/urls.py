8a4726d3642bb332878a57cdd60bbf1eb4683e7c
MokkoFm <mokkofmpoetry@gmail.com>
2020-11-15 02:26:57 +0200

    add base.html, add static for all pages

--- spaweb/urls.py
+++ spaweb/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('contact', views.contact, name="contact"),
     path('login', views.login, name="login"),
     path('product/<slug:slug>', views.product_detail, name="product-detail"),
+    path('product-listing', views.product_listing, name="product-listing"),
+    path('register', views.register, name="register"),
+    path('cart', views.cart, name="cart"),
+    path('standalone', views.standalone, name="standalone")
 ]
--- spaweb/views.py
+++ spaweb/views.py
@@ -15,3 +15,19 @@ def login(request):
 
 def product_detail(request):
     return render(request, "product-detail.html")
+
+
+def product_listing(request):
+    return render(request, 'product-listing.html')
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def cart(request):
+    return render(request, "shop-cart.html")
+
+
+def standalone(request):
+    return render(request, "standalone.html")
