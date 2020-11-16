5df883b6c32754c2d2840f9d9d9f65c8dc28217b
MokkoFm <mokkofmpoetry@gmail.com>
2020-11-16 21:03:19 +0200

    add correct urls

--- spaweb/urls.py
+++ spaweb/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('contact', views.contact, name="contact"),
-    path('login', views.login, name="login"),
-    path('product/<slug:slug>', views.product_detail, name="product-detail"),
-    path('category/<slug:slug>', views.product_listing, name="product-listing"),
-    path('register', views.register, name="register"),
-    path('cart', views.cart, name="cart"),
-    path('standalone', views.standalone, name="standalone")
+    path('contact/', views.contact, name="contact"),
+    path('login/', views.login, name="login"),
+    path('product/<slug:slug>/', views.product_detail, name="product-detail"),
+    path('category/<slug:slug>/', views.product_listing, name="product-listing"),
+    path('register/', views.register, name="register"),
+    path('cart/', views.cart, name="cart"),
+    path('standalone/', views.standalone, name="standalone")
 ]
