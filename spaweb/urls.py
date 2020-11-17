792a61037d6f81b6f8b8c9845c0cf7a785220fae
andimeon <andimeon@gmail.com>
2020-11-17 23:06:51 +0900

    Implement product output

--- spaweb/urls.py
+++ spaweb/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from . import views
+from spaweb import views
 
 urlpatterns = [
     path('', views.index, name="index"),
     path('contact/', views.contact, name="contact"),
-    path('login', views.login, name="login"),
+    path('login/', views.login, name="login"),
     path('product/<slug:slug>', views.product_detail, name="product-detail"),
     path('category/<slug:slug>', views.product_listing, name="product-listing"),
-    path('register', views.register, name="register"),
-    path('cart', views.cart, name="cart"),
-    path('standalone', views.standalone, name="standalone")
+    path('register/', views.register, name="register"),
+    path('cart/', views.cart, name="cart"),
+    path('standalone/', views.standalone, name="standalone")
 ]
