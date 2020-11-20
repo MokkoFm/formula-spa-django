d3ae1838b3e513e6d6c1da77b3a5d484e51fdb71
andimeon <andimeon@gmail.com>
2020-11-20 13:38:39 +0900

    Unused script

--- spaweb/custom_context_processor.py
+++
@@ -1,9 +0,0 @@
-from spaweb.models import BusinessDirection
-
-
-def add_variable_to_context(request):
-    programs = BusinessDirection.objects.filter(title="ПРОГРАММЫ")
-    context = {
-        'programs': programs,
-    }
-    return context
