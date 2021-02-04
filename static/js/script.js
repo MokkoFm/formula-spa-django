054e0de95474d70e278601896ac71b9190c4789f
MokkoFm <mokkofmpoetry@gmail.com>
2021-02-05 00:21:58 +0200

    update owl-carousel for certificates

--- static/js/script.js
+++ static/js/script.js
@@ -361,6 +361,28 @@ jQuery(function ($) {
         }
     });
 
+    $('.certificates').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+
 
     /*===================================
         SEARCH BOX MEDIA ITEAMS
