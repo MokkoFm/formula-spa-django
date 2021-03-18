1919b99f24fcc8584b6bf0f74d3220e8268797b8
MokkoFm <mokkofmpoetry@gmail.com>
2021-03-18 14:14:11 +0200

    update settings for static files

--- formulaspa/urls.py
+++ formulaspa/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('spaweb.urls')),
     path('tinymce/', include('tinymce.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
